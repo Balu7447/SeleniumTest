89f855f53ea48956e7d3a5ccf457191fd30419fd
Balu7447 <50898490+Balu7447@users.noreply.github.com>
2019-11-18 11:37:24 +0530

    Update MyClass.java

--- src/newpackage/MyClass.java
+++ src/newpackage/MyClass.java
@@ -17,9 +17,6 @@ public class MyClass {
 		
 		driver.manage().window().maximize();
 		
-//		driver.findElement(By.xpath("//input[@id='standard-name']")).sendKeys("balu.cse2@gmail.com");
-		
-//		driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
 		
 		Thread.sleep(3000);
 		
@@ -55,26 +52,6 @@ public class MyClass {
 		
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("(//img[@src='https://via.placeholder.com/340x490.png?text=%20'])[1]")).click();
-		//driver.findElement(By.xpath("//div[@class='image']")).click();
-		
-		
-		
-
-		
-//		Thread.sleep(3000);
-//		driver.findElement(By.cssSelector("#root > button")).click();
-		
-//		driver.get("https://sc-platform-stage.supplycompass.com/collections/create");
-		
-		
-		
-		/*driver.findElement(By.xpath("//button[@class='sc-bxivhb jtFfkZ']")).click();
-		
-			driver.navigate().to("mail.google.com");*/
-		//WebElement password = driver.findElement(By.id("passwd"));
-		
-//		password.sendKeys("rgukt123");
-		
 		
 	}
 	
